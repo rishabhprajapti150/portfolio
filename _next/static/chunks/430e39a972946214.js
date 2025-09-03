cf964097da9c4e1adce061e80f4eccca14937b8e
RishabhEdulab <rishabhp@edulab.in>
2025-09-03 19:33:13 +0530

    Updates

---
+++ _next/static/chunks/430e39a972946214.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/db446d9bdee298ca.js",
+  "static/chunks/turbopack-7f2e47c813f9dfb9.js"
+])
---
+++ _next/static/ewppOujSXogGSRZUixOyO/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/97fbb23bdc702ade.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/portfolio//_next/:path+",
+        "destination": "/portfolio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
