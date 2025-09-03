52b1313dd2af6992381823ed405d151d2c534ee4
RishabhEdulab <rishabhp@edulab.in>
2025-09-03 20:38:36 +0530

    RP-FE-allow to download resume

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -71,7 +71,7 @@ export default function Header() {
                 </nav>
                 <div className="mt-auto">
                   <a
-                    href="/Rishabh_Fullstack.pdf"
+                    href="/portfolio/Rishabh_Fullstack.pdf"
                     download
                   >
                     <Button className="w-full">
