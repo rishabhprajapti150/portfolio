e4a3ed3379dbf1af686523a7fdc6c48a3ab06722
RishabhEdulab <rishabhp@edulab.in>
2025-09-03 19:52:06 +0530

    Updates

--- _next/static/chunks/3625c3b392796317.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/03a18b7a7702d169.js",
-  "static/chunks/turbopack-e8bb16cf6c8a66c3.js"
-])
--- _next/static/chunks/db7e483bfb94eb42.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/03a18b7a7702d169.js",
-  "static/chunks/turbopack-b0cc8d6b2d3b1224.js"
-])
