2da9720da6d1a3e40432a995263729447686ded2
RishabhEdulab <rishabhp@edulab.in>
2025-09-03 19:42:20 +0530

    correct the path

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,41 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   output: "export",
+//   /* config options here */
+//   images: {
+//     unoptimized: true,
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'placehold.co',
+//         port: '',
+//         pathname: '/**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'picsum.photos',
+//         port: '',
+//         pathname: '/**',
+//       },
+//     ],
+//   },
+//   // distDir: "dist",
+//   basePath: '/portfolio',      // repo name
+//   assetPrefix: '/portfolio/',  // repo name
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  /* config options here */
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
   },
-  // distDir: "dist",
-  basePath: '/portfolio',      // repo name
-  assetPrefix: '/portfolio/',  // repo name
+  basePath: "/portfolio",      // repo name
+  assetPrefix: "/portfolio/",  // repo name
 };
 
 export default nextConfig;
+
