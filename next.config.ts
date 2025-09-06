851b3c9f5fbd98bee0b6a364d0e6073b47441147
RishabhEdulab <rishabhp@edulab.in>
2025-09-06 12:54:55 +0530

    RP-FIX-fix the build error

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export",
-  images: { unoptimized: true },
-  basePath: "/portfolio",
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // replaces old `next export`
+  images: {
+    unoptimized: true, // disable Image Optimization (needed for static hosting)
+  },
+  basePath: "/portfolio", // 👈 add this since your repo is rishabhprajapti150.github.io/portfolio
   assetPrefix: "/portfolio/",
-  trailingSlash: true
 };
 
-export default nextConfig;
+module.exports = nextConfig;
